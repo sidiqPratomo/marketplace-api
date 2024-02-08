5bba4fe83cc57e24965c38eae1e659278f386ad1
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-02-08 17:46:02 +0700

    add create user by role hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,17 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { email, name, password } = req.body;
+  const emailAlreadyExist = await User.findOne({ email });
+  if (emailAlreadyExist) {
+    throw new CustomError.BadRequestError("Email already exist");
+  }
+
+  //first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" :"user";
+
+  const user = await User.create({name, email, password, role});
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -9,6 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'Please provide email'],
         validate:{
             validator: validator.isEmail,
@@ -25,6 +28,16 @@ const userSchema = new mongoose.Schema({
         enum: ['admin', 'user'],
         default: 'user',
     }
+});
+
+userSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 })
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
 
 module.exports = mongoose.model('User', userSchema);
