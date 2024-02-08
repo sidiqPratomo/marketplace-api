29d430f3bae687489c4f131bdbc869674225e229
pratomosidiq123 <pratomosidiq123@gmail.com>
2024-02-08 19:11:57 +0700

    add jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const { createJWT, isTokenValid } = require("../utils");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -11,10 +12,13 @@ const register = async (req, res) => {
 
   //first registered user is an admin
   const isFirstAccount = (await User.countDocuments({})) === 0;
-  const role = isFirstAccount ? "admin" :"user";
+  const role = isFirstAccount ? "admin" : "user";
 
-  const user = await User.create({name, email, password, role});
-  res.status(StatusCodes.CREATED).json({ user });
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token  =  createJWT({payload: tokenUser})
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 
 const login = async (req, res) => {
